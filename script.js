87270191dd14580f6d0f9583fc8fc6d5ced8b2f3
Buranch <birukmisanaw@gmail.com>
2023-03-08 14:11:37 -0800

    Make the password visibility toggle work

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
+  function togglePassword(id) {
+    var x = document.getElementById(id);
+    if (x.type === "password") {
+      x.type = "text";
+      $(`#${id}-hidden-eye`).css("display", "block");
+      $(`#${id}-visible-eye`).css("display", "none");
+    } else {
+      x.type = "password";
+      $(`#${id}-visible-eye`).css("display", "block");
+      $(`#${id}-hidden-eye`).css("display", "none");
+    }
+  }
+
   $("#minimize-btn").click(() => {
     $(".claim-info").css("visibility", "hidden");
+  });
+
+  $("#show-password-toggle").click(() => {
+    togglePassword("pass1");
   });
 });
